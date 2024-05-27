a0fde54a67998229373b89bf3ed918c7398da865
tachitetsuya <tachi.tetsuya88@gmail.com>
2024-05-27 16:54:02 +0900

    test

--- select_a5.py
+++ select_a5.py
@@ -1,11 +1,14 @@
 import openpyxl
 
 def select_a1_in_all_sheets(file_path):
-    workbook = openpyxl.load_workbook(file_path)
-    for sheet in workbook.worksheets:
-        sheet.sheet_view.topLeftCell = 'A1'
-        sheet.sheet_view.selection = [openpyxl.worksheet.views.Selection(sqref='A1')]
-    workbook.save(file_path)
+    try:
+        workbook = openpyxl.load_workbook(file_path)
+        for sheet in workbook.worksheets:
+            sheet.sheet_view.topLeftCell = 'A1'
+            sheet.sheet_view.selection = [openpyxl.worksheet.views.Selection(sqref='A1')]
+        workbook.save(file_path)
+    except Exception as e:
+        print(f"e {e]")
 
 if __name__ == "__main__":
     excel_file_path = 'test3.xlsx'
