46f27af671b29baf5e98137870420e869c57e064
tachitetsuya <tachi.tetsuya88@gmail.com>
2024-05-27 11:22:54 +0900

    test

--- select_a3.py
+++ select_a3.py
@@ -1,7 +1,6 @@
 import openpyxl
 
 def select_a1_in_all_sheets(file_path):
-    # Excelファイルを開く
     workbook = openpyxl.load_workbook(file_path)
     for sheet in workbook.worksheets:
         sheet.sheet_view.selection = [
@@ -9,10 +8,8 @@ def select_a1_in_all_sheets(file_path):
                 pane='topLeft', activeCell='A1', sqref='A1'
             )
         ]
-    # ファイルを保存
     workbook.save(file_path)
 
 if __name__ == "__main__":
-    # Excelファイルのパスを指定
     excel_file_path = 'test3.xlsx'
     select_a1_in_all_sheets(excel_file_path)
